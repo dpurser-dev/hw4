1a4e1efdcaf66d7561d16f957f160efeae36d367
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-05-27 22:03:58 +0000

    Initial commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,31 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Incorrect password"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "User not found"
+      redirect_to "/login"
+    end
+
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logout successful"
+    redirect_to "/login"
   end
+
 end
   
