c2137619ed46509e88dd791607c80c239d1db34e
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-06-08 00:05:21 +0000

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources "posts"
   resources "sessions"
   resources "users"
-  
+
   get "/", :controller => "places", :action => "index"
   get "/login", :controller => "sessions", :action => "new"
   get "/logout", :controller => "sessions", :action => "destroy"
